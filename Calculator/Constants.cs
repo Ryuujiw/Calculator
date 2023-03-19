1aff17d6adfa09cad3d6def60f7fd0333c235235
rujinw <rujinwong@gmail.com>
2023-03-19 12:58:27 +0800

    Implement shunting yard to convert infix to postfix

---
+++ Calculator/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculator
+{
+    public static class Constants
+    {
+        public const char LEFT_PAREN = '(';
+        public const char RIGHT_PAREN = ')';
+        public const char OP_ADD = '+';
+        public const char OP_MINUS = '-';
+        public const char OP_MULTIPLY = '*';
+        public const char OP_DIVIDE = '/';
+        public const char OP_DECIMAL = '.';
+    }
+}
---
+++ Calculator/ShuntingYard.cs
@@ -0,0 +1,59 @@
+﻿namespace Calculator
+{
+    public static class ShuntingYard
+    {
+        public static string ConvertToPostfix(string input)
+        {
+            string postfix = string.Empty;
+            var stack = new Stack<char>();
+
+            foreach (var c in input)
+            {
+                if (char.IsLetterOrDigit(c) || c == Constants.OP_DECIMAL || char.IsWhiteSpace(c))
+                {
+                    postfix += c;
+                }
+                else if (c == Constants.LEFT_PAREN)
+                {
+                    stack.Push(c);
+                }
+                else if (c == Constants.RIGHT_PAREN)
+                {
+                    while (stack.Count > 0 && stack.Peek() != Constants.LEFT_PAREN)
+                    {
+                        postfix += $" {stack.Pop()}";
+                    }
+                    stack.Pop();
+                }
+                else // OPERATOR
+                {
+                    while (stack.Count > 0 && Precedence(c) <= Precedence(stack.Peek()))
+                    {
+                        postfix += $" {stack.Pop()}";
+                    }
+                    stack.Push(c);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                postfix += $" {stack.Pop()}";
+            }
+
+            return postfix;
+        }
+
+        private static int Precedence(char c)
+        {
+            if (c == Constants.OP_ADD || c == Constants.OP_MINUS)
+            {
+                return 1;
+            }
+            if (c == Constants.OP_MULTIPLY || c == Constants.OP_DIVIDE)
+            {
+                return 2;
+            }
+            return -1;
+        }
+    }
+}
