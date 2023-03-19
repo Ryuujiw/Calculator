ce58077424a92e7b4db0b3575be012c2eed6eee9
rujinw <rujinwong@gmail.com>
2023-03-19 13:16:08 +0800

    Add a basic console to allow user to input + error handling

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -4,24 +4,35 @@
     {
         public double Calculate(string sum)
         {
-            var postfixExpression = ShuntingYard.ConvertToPostfix(sum);
-            var stack = new Stack<double>();
-
-            foreach (var token in postfixExpression.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            try
             {
-                if (double.TryParse(token, out var value))
-                {
-                    stack.Push(value);
-                }
-                else
-                {
-                    var rhs = stack.Pop();
-                    var lhs = stack.Pop();
-                    stack.Push(Calculate(lhs, rhs, char.Parse(token)));
-                }
-            }
+                var postfixExpression = ShuntingYard.ConvertToPostfix(sum);
+                var stack = new Stack<double>();
 
-            return stack.Pop();
+                foreach (var token in postfixExpression.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    if (double.TryParse(token, out var value))
+                    {
+                        stack.Push(value);
+                    }
+                    else
+                    {
+                        if (stack.Count < 2)
+                        {
+                            throw new Exception("Invalid expression");
+                        }
+                        var rhs = stack.Pop();
+                        var lhs = stack.Pop();
+                        stack.Push(Calculate(lhs, rhs, char.Parse(token)));
+                    }
+                }
+
+                return stack.Pop();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         private static double Calculate(double lhs, double rhs, char op)
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string userInput;
+Console.WriteLine("Enter an expression, x to exit - ");
+userInput = Console.ReadLine();
+
+while (userInput != "x")
+{
+    var calculator = new Calculator.Calculator();
+    double computedResult = 0D;
+    try
+    {
+        computedResult = calculator.Calculate(userInput);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine("Enter an expression, x to exit - ");
+        userInput = Console.ReadLine();
+        continue;
+    }
+    Console.WriteLine("You entered: '{0}'", userInput);
+    Console.WriteLine("Result: '{0}'", computedResult);
+
+    Console.WriteLine("Enter an expression, x to exit - ");
+    userInput = Console.ReadLine();
+}
